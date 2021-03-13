e99089486825dc6185f6429c2dde938003cb26b1
陈梦晨 <yiyouguisu@163.com>
2021-03-13 17:03:55 +0800

    init commit

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"gRPCServerDemo/chat"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	gRPCServer := grpc.NewServer()
+
+	s := chat.Server{}
+	chat.RegisterChatServiceServer(gRPCServer, &s)
+
+	if err := gRPCServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+}
